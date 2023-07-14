2193271a44f151c3976ebbfd475ce844581dd783
Payton Reed <reedpayton23@gmail.com>
2023-07-14 12:51:10 -0500

    Fixed a few responses in the methods, since I called it wrong

--- generating_responses.py
+++ generating_responses.py
@@ -15,18 +15,34 @@ class GenerateResponse:
         return f"You liked {self.calculate.count_likes()} vidoes"
         
     def total_login_history_response(self):
-        return f"You logged in {self.calculate.login_history()} times"
+        return f"You logged in {self.calculate.count_login_history()} times"
     
     def total_share_history_response(self):
-        return f"You shared {self.calculate.share_history()} vidoes"
+        return f"You shared {self.calculate.count_share_history()} vidoes"
     
     def total_comments_response(self):
-        return f"You commented {self.calculate.comments_list()} times"
+        return f"You commented {self.calculate.count_comments()} times"
     
     def total_watched_tiktok_live_response(self):
-        return f"You watched {self.calculate.watch_tiktok_live_history()} \
+        return f"You watched {self.calculate.count_watched_tiktok_live()} \
             TikTok lives"
     
     def total_vidoes_you_made_response(self):
         return f"You made {self.calculate.count_videos_you_made()} vidoes"
 
+"""
+def main():
+    '''
+    Runs all the methods in the class above
+    '''
+    response = GenerateResponse()
+    
+    print(response.total_likes_response())
+    print(response.total_login_history_response())
+    print(response.total_share_history_response())
+    print(response.total_comments_response())
+    print(response.total_vidoes_you_made_response())
+   
+main()
+"""
+    
